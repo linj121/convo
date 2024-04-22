510e3ec817f89ccafa44b61f5b295b08be0e333f
linj121 <jiexulin99@gmail.com>
2024-04-22 17:01:18 -0400

    feat: log group chat trigger rule

--- src/bot.ts
+++ src/bot.ts
@@ -1,8 +1,13 @@
 import { config } from "./config.js";
-import {ContactImpl, ContactInterface, RoomImpl, RoomInterface} from "wechaty/impls";
+import {
+  ContactImpl,
+  ContactInterface,
+  RoomImpl,
+  RoomInterface,
+} from "wechaty/impls";
 import { Message } from "wechaty";
-import {FileBox} from "file-box";
-import {chatgpt, dalle, whisper} from "./openai.js";
+import { FileBox } from "file-box";
+import { chatgpt, dalle, whisper } from "./openai.js";
 import DBUtils from "./data.js";
 import { regexpEncode } from "./utils.js";
 enum MessageType {
@@ -26,14 +31,16 @@ enum MessageType {
 }
 const SINGLE_MESSAGE_MAX_SIZE = 500;
 type Speaker = RoomImpl | ContactImpl;
-interface ICommand{
-  name:string;
-  description:string;
-  exec: (talker:Speaker, text:string) => Promise<void>;
+interface ICommand {
+  name: string;
+  description: string;
+  exec: (talker: Speaker, text: string) => Promise<void>;
 }
 export class ChatGPTBot {
   chatPrivateTriggerKeyword = config.chatPrivateTriggerKeyword;
-  chatTriggerRule = config.chatTriggerRule? new RegExp(config.chatTriggerRule): undefined;
+  chatTriggerRule = config.chatTriggerRule
+    ? new RegExp(config.chatTriggerRule)
+    : undefined;
   disableGroupMessage = config.disableGroupMessage || false;
   botName: string = "";
   ready = false;
@@ -45,28 +52,31 @@ export class ChatGPTBot {
   }
   get chatPrivateTriggerRule(): RegExp | undefined {
     const { chatPrivateTriggerKeyword, chatTriggerRule } = this;
-    let regEx = chatTriggerRule
+    let regEx = chatTriggerRule;
     if (!regEx && chatPrivateTriggerKeyword) {
-      regEx = new RegExp(regexpEncode(chatPrivateTriggerKeyword))
+      regEx = new RegExp(regexpEncode(chatPrivateTriggerKeyword));
     }
-    return regEx
+    return regEx;
   }
-  private readonly commands:ICommand[] = [
+  private readonly commands: ICommand[] = [
     {
       name: "help",
       description: "显示帮助信息",
       exec: async (talker) => {
-        await this.trySay(talker,"========\n" +
-          "/cmd help\n" +
-          "# 显示帮助信息\n" +
-          "/cmd prompt <PROMPT>\n" +
-          "# 设置当前会话的 prompt \n" +
-          "/img <PROMPT>\n" +
-          "# 根据 prompt 生成图片\n" +
-          "/cmd clear\n" +
-          "# 清除自上次启动以来的所有会话\n" +
-          "========");
-      }
+        await this.trySay(
+          talker,
+          "========\n" +
+            "/cmd help\n" +
+            "# 显示帮助信息\n" +
+            "/cmd prompt <PROMPT>\n" +
+            "# 设置当前会话的 prompt \n" +
+            "/img <PROMPT>\n" +
+            "# 根据 prompt 生成图片\n" +
+            "/cmd clear\n" +
+            "# 清除自上次启动以来的所有会话\n" +
+            "========"
+        );
+      },
     },
     {
       name: "prompt",
@@ -74,10 +84,10 @@ export class ChatGPTBot {
       exec: async (talker, prompt) => {
         if (talker instanceof RoomImpl) {
           DBUtils.setPrompt(await talker.topic(), prompt);
-        }else {
+        } else {
           DBUtils.setPrompt(talker.name(), prompt);
         }
-      }
+      },
     },
     {
       name: "clear",
@@ -85,12 +95,12 @@ export class ChatGPTBot {
       exec: async (talker) => {
         if (talker instanceof RoomImpl) {
           DBUtils.clearHistory(await talker.topic());
-        }else{
+        } else {
           DBUtils.clearHistory(talker.name());
         }
-      }
-    }
-  ]
+      },
+    },
+  ];
 
   /**
    * EXAMPLE:
@@ -121,18 +131,18 @@ export class ChatGPTBot {
     const { chatTriggerRule, chatPrivateTriggerRule } = this;
 
     if (privateChat && chatPrivateTriggerRule) {
-      text = text.replace(chatPrivateTriggerRule, "")
+      text = text.replace(chatPrivateTriggerRule, "");
     } else if (!privateChat) {
-      text = text.replace(this.chatGroupTriggerRegEx, "")
-      text = chatTriggerRule? text.replace(chatTriggerRule, ""): text
+      text = text.replace(this.chatGroupTriggerRegEx, "");
+      text = chatTriggerRule ? text.replace(chatTriggerRule, "") : text;
     }
     // remove more text via - - - - - - - - - - - - - - -
-    return text
+    return text;
   }
-  async getGPTMessage(talkerName: string,text: string): Promise<string> {
-    let gptMessage = await chatgpt(talkerName,text);
-    if (gptMessage !=="") {
-      DBUtils.addAssistantMessage(talkerName,gptMessage);
+  async getGPTMessage(talkerName: string, text: string): Promise<string> {
+    let gptMessage = await chatgpt(talkerName, text);
+    if (gptMessage !== "") {
+      DBUtils.addAssistantMessage(talkerName, gptMessage);
       return gptMessage;
     }
     return "Sorry, please try again later. 😔";
@@ -169,13 +179,15 @@ export class ChatGPTBot {
     const { chatTriggerRule } = this;
     let triggered = false;
     if (privateChat) {
-      const regEx = this.chatPrivateTriggerRule
-      triggered = regEx? regEx.test(text): true;
+      const regEx = this.chatPrivateTriggerRule;
+      triggered = regEx ? regEx.test(text) : true;
     } else {
       triggered = this.chatGroupTriggerRegEx.test(text);
       // group message support `chatTriggerRule`
       if (triggered && chatTriggerRule) {
-        triggered = chatTriggerRule.test(text.replace(this.chatGroupTriggerRegEx, ""))
+        triggered = chatTriggerRule.test(
+          text.replace(this.chatGroupTriggerRegEx, "")
+        );
       }
     }
     if (triggered) {
@@ -215,7 +227,7 @@ export class ChatGPTBot {
   }
 
   async onPrivateMessage(talker: ContactInterface, text: string) {
-    const gptMessage = await this.getGPTMessage(talker.name(),text);
+    const gptMessage = await this.getGPTMessage(talker.name(), text);
     await this.trySay(talker, gptMessage);
   }
 
@@ -224,7 +236,7 @@ export class ChatGPTBot {
     text: string,
     room: RoomInterface
   ) {
-    const gptMessage = await this.getGPTMessage(await room.topic(),text);
+    const gptMessage = await this.getGPTMessage(await room.topic(), text);
     const result = `@${talker.name()} ${text}\n\n------\n ${gptMessage}`;
     await this.trySay(room, result);
   }
@@ -235,50 +247,52 @@ export class ChatGPTBot {
     const messageType = message.type();
     const privateChat = !room;
     if (privateChat) {
-      console.log(`🤵 Contact: ${talker.name()} 💬 Text: ${rawText}`)
+      console.log(`🤵 Contact: ${talker.name()} 💬 Text: ${rawText}`);
     } else {
-      const topic = await room.topic()
-      console.log(`🚪 Room: ${topic} 🤵 Contact: ${talker.name()} 💬 Text: ${rawText}`)
+      const topic = await room.topic();
+      console.log(
+        `🚪 Room: ${topic} 🤵 Contact: ${talker.name()} 💬 Text: ${rawText}`
+      );
     }
     if (this.isNonsense(talker, messageType, rawText)) {
       return;
     }
-    if (messageType == MessageType.Audio){
+    if (messageType == MessageType.Audio) {
       // 保存语音文件
       const fileBox = await message.toFileBox();
       let fileName = "./public/" + fileBox.name;
       await fileBox.toFile(fileName, true).catch((e) => {
-        console.log("保存语音失败",e);
+        console.log("保存语音失败", e);
         return;
       });
       // Whisper
-      whisper("",fileName).then((text) => {
+      whisper("", fileName).then((text) => {
         message.say(text);
-      })
+      });
       return;
     }
-    if (rawText.startsWith("/cmd ")){
-      console.log(`🤖 Command: ${rawText}`)
-      const cmdContent = rawText.slice(5) // 「/cmd 」一共5个字符(注意空格)
+    if (rawText.startsWith("/cmd ")) {
+      console.log(`🤖 Command: ${rawText}`);
+      const cmdContent = rawText.slice(5); // 「/cmd 」一共5个字符(注意空格)
       if (privateChat) {
         await this.command(talker, cmdContent);
-      }else{
+      } else {
         await this.command(room, cmdContent);
       }
       return;
     }
     // 使用DallE生成图片
-    if (rawText.startsWith("/img")){
-      console.log(`🤖 Image: ${rawText}`)
-      const imgContent = rawText.slice(4)
+    if (rawText.startsWith("/img")) {
+      console.log(`🤖 Image: ${rawText}`);
+      const imgContent = rawText.slice(4);
       if (privateChat) {
-        let url = await dalle(talker.name(), imgContent) as string;
-        const fileBox = FileBox.fromUrl(url)
-        message.say(fileBox)
-      }else{
-        let url = await dalle(await room.topic(), imgContent) as string;
-        const fileBox = FileBox.fromUrl(url)
-        message.say(fileBox)
+        let url = (await dalle(talker.name(), imgContent)) as string;
+        const fileBox = FileBox.fromUrl(url);
+        message.say(fileBox);
+      } else {
+        let url = (await dalle(await room.topic(), imgContent)) as string;
+        const fileBox = FileBox.fromUrl(url);
+        message.say(fileBox);
       }
       return;
     }
@@ -286,8 +300,8 @@ export class ChatGPTBot {
       const text = this.cleanMessage(rawText, privateChat);
       if (privateChat) {
         return await this.onPrivateMessage(talker, text);
-      } else{
-        if (!this.disableGroupMessage){
+      } else {
+        if (!this.disableGroupMessage) {
           return await this.onGroupMessage(talker, text, room);
         } else {
           return;
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import { WechatyBuilder } from "wechaty";
 import QRCode from "qrcode";
 import { ChatGPTBot } from "./bot.js";
-import {config} from "./config.js";
+import { config } from "./config.js";
 const chatGPTBot = new ChatGPTBot();
 
-const bot =  WechatyBuilder.build({
+const bot = WechatyBuilder.build({
   name: "wechat-assistant", // generate xxxx.memory-card.json and save login data for the next login
   puppet: "wechaty-puppet-wechat",
   puppetOptions: {
-    uos: true
-  }
+    uos: true,
+  },
 });
 async function main() {
-  const initializedAt = Date.now()
+  const initializedAt = Date.now();
   bot
     .on("scan", async (qrcode, status) => {
       const url = `https://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`;
@@ -24,9 +24,16 @@ async function main() {
     .on("login", async (user) => {
       chatGPTBot.setBotName(user.name());
       console.log(`User ${user} logged in`);
+      console.log(
+        `群聊触发关键词: @${chatGPTBot.botName} + ${config.chatTriggerRule}`
+      );
       console.log(`私聊触发关键词: ${config.chatPrivateTriggerKeyword}`);
-      console.log(`已设置 ${config.blockWords.length} 个聊天关键词屏蔽. ${config.blockWords}`);
-      console.log(`已设置 ${config.chatgptBlockWords.length} 个ChatGPT回复关键词屏蔽. ${config.chatgptBlockWords}`);
+      console.log(
+        `已设置 ${config.blockWords.length} 个聊天关键词屏蔽. ${config.blockWords}`
+      );
+      console.log(
+        `已设置 ${config.chatgptBlockWords.length} 个ChatGPT回复关键词屏蔽. ${config.chatgptBlockWords}`
+      );
     })
     .on("message", async (message) => {
       if (message.date().getTime() < initializedAt) {
